b2d796274dad884aa0e1b86d728c88d2e3f9087e
Igor <Sketch_90@mail.ru>
2023-01-20 18:23:26 +0300

    Testing

--- src/main/java/com/example/web/HelloController.java
+++ src/main/java/com/example/web/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 
     //@RequestMapping(value = "/", method = RequestMethod.GET)
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String defaultPage(ModelMap model) {
         return "hello";
     }
